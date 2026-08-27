9f6db03f7adbcf9aa5dac0df5854810bfba95009
mammadov115 <mehman.mammadov182@gmail.com>
2026-08-27 13:04:24 +0400

    feat: add PostRepository with custom status-based query method

---
+++ src/main/java/com/spring/blog/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.spring.blog.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.blog.model.Post;
+import com.spring.blog.model.Status;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByStatusOrderByPublishDesc(Status status);
+}
