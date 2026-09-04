489edc00dfe333ba48901852a53f816977b76eb8
mammadov115 <mehman.mammadov182@gmail.com>
2026-09-04 17:02:23 +0400

    feat: add @EnableAsync annotation to BlogApplication and EmailService for asynchronous processing

--- src/main/java/com/spring/blog/service/EmailService.java
+++ src/main/java/com/spring/blog/service/EmailService.java
@@ -3,8 +3,8 @@ package com.spring.blog.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
 
 import com.spring.blog.dto.PostResponse;
 import com.spring.blog.dto.SharePostRequest;
@@ -19,20 +19,21 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Async
     public void sharePost(PostResponse post, SharePostRequest request) {
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setFrom(fromEmail);
-    message.setTo(request.getRecipientEmail());
-    message.setSubject(request.getSenderName() + " shared with you: " + post.title());
-    message.setText(
-        request.getSenderName() + " (" + request.getSenderEmail() + ") " +
-        "share post with you:\n\n" +
-        post.title() + "\n\n" +
-        "http://localhost:8080/api/posts/" + post.slug() +
-        (request.getComment() != null && !request.getComment().isBlank()
-            ? "\n\nŞərh: " + request.getComment()
-            : "")
-    );
-    mailSender.send(message);
-}
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(request.getRecipientEmail());
+        message.setSubject(request.getSenderName() + " shared with you: " + post.title());
+        message.setText(
+            request.getSenderName() + " (" + request.getSenderEmail() + ") " +
+            "share post with you:\n\n" +
+            post.title() + "\n\n" +
+            "http://localhost:8080/api/posts/" + post.slug() +
+            (request.getComment() != null && !request.getComment().isBlank()
+                ? "\n\nŞərh: " + request.getComment()
+                : "")
+        );
+        mailSender.send(message);
+    }
 }
